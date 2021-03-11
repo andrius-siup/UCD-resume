d4b0725a1d8c67b231a9e650ae2a615249e3c341
andrius-siup <andrius.siupinys@gmail.com>
2021-03-11 20:26:58 +0000

    Fix everything and works fine.

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,35 +1,18 @@
 function sendMail(contactForm) {
-    email.send("Gmail", "rosie", {
+    emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
-    }, user_Hft1f0wwhp6PvNa61wBb2)
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
-       // return false;
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+     // block from loading new page   
+    //return false;  
 }
-// var templateParams = {
-//     name: 'James',
-//     notes: 'Check this out!'
-// };
 
-// emailjs.send('Gmail', 'rosie', templateParams)
-//     .then(function(response) {
-//        console.log('SUCCESS!', response.status, response.text);
-//     }, function(error) {
-//        console.log('FAILED...', error);
-//     });
-
-    // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', '#myForm')
-    // .then(function(response) {
-    //    console.log('SUCCESS!', response.status, response.text);
-    // }, function(error) {
-    //    console.log('FAILED...', error);
-    // });
-
-console.log("hello");
